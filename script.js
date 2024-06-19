5fd2f2b3e8ad91e7b007fcab7f884e59146b9035
JanHamo <jan.hamou@hotmail.com>
2024-06-19 21:18:29 +0200

    Minor adjustments to text and styles

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function changeText() {
     const heading = document.querySelector("h1");
-    heading.textContent = "Text changed!";
+    heading.textContent = "Text changed! Welcome to the new version!";
+    heading.style.color = "red"; // Changed text color
 }
